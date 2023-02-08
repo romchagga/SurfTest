b181bf36afb30fbf59f7060bec44addf6bf20b5d
Ramis Askerov <askerov.ramis1@gmail.com>
2023-02-08 22:04:14 +0300

    add constraint animate and little config customView

---
+++ SurfTest/BottomContainerView.swift
@@ -0,0 +1,58 @@
+//
+//  BottomContainerView.swift
+//  SurfTest
+//
+//  Created by macbook on 08.02.2023.
+//
+
+import UIKit
+
+class BottomContainerView: UIView {
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Стажировка в Surf"
+        label.font = UIFont(name: "SFProDisplay-Bold", size: 24)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Работай над реальными задачами под руководством опытного наставника и получи возможность стать частью команды мечты."
+        label.numberOfLines = 0
+        label.textColor = .gray
+        label.font = UIFont(name: "SFProDisplay-Regular", size: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = .white
+        setConstraints()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func setConstraints() {
+        self.addSubview(titleLabel)
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor,constant: 20),
+            titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 24)
+        ])
+        
+        self.addSubview(descriptionLabel)
+        NSLayoutConstraint.activate([
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 12),
+            descriptionLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            descriptionLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20)
+        ])
+    }
+    
+}
--- SurfTest/ViewController.swift
+++ SurfTest/ViewController.swift
@@ -8,12 +8,91 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "customImage")
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let separatorView: UIView = {
+        let view = UIView()
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 15
+        view.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+        view.backgroundColor = .clear
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    let bottomContainerView : BottomContainerView = {
+        let view = BottomContainerView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    var centerConstraint: NSLayoutConstraint!
+
+    var startingConstant: CGFloat  = 0.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setConstraints()
+        configGesture()
+    }
+    
+    @objc func detectPan(recognizer: UIPanGestureRecognizer) {
+
+        switch recognizer.state {
+        case .began:
+            self.startingConstant = self.centerConstraint.constant
+        case .changed:
+            let translation = recognizer.translation(in: self.view)
+            self.centerConstraint.constant = self.startingConstant + translation.y
+        default:
+            break
+        }
+    }
+    
+    func configGesture() {
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(detectPan(recognizer:)))
+        panGesture.delaysTouchesBegan = false
+        panGesture.delaysTouchesEnded = false
+        separatorView.addGestureRecognizer(panGesture)
     }
 
+    func setConstraints() {
 
+        view.addSubview(imageView)
+        view.addSubview(bottomContainerView)
+        view.addSubview(separatorView)
+
+        separatorView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        separatorView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+
+        
+        self.centerConstraint = separatorView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        self.centerConstraint.isActive = true
+        
+        separatorView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        //separatorView.topAnchor.constraint(equalTo: view.centerYAnchor, constant: 150).isActive = true
+
+        imageView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        imageView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: separatorView.topAnchor).isActive = true
+        
+
+        bottomContainerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        bottomContainerView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        bottomContainerView.topAnchor.constraint(equalTo: separatorView.topAnchor).isActive = true
+        bottomContainerView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    
+    }
+    
 }
 
